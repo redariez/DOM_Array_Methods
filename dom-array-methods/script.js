1ffa28aba10b641615b37e1e27ac5ffcfbf3cb6e
ade bakari <redariez@gmail.com>
2020-05-29 12:34:24 -0700

    dom array methods complete

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -1,73 +1,96 @@
- const main = document.getElementById('main');
- const addUserBtn = document.getElementById('add-user');
- const doubleBtn = document.getElementById('double');
- const showMillionaires = document.getElementById('show-millionaires');
- const sortBtn = document.getElementById('sort');
- const calculateWealthBtn = document.getElementById('calculate-wealth');
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
 
- let data = [];
+let data = [];
 
- getRandomUser();
- getRandomUser();
- getRandomUser();
- 
- // fetch random user and add money
- async function getRandomUser() {
-   const res = await fetch('https://randomuser.me/api');
-   const data = await res.json();
+getRandomUser();
+getRandomUser();
+getRandomUser();
 
-   const user = data.results[0];
+// Fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
 
-   const newUser = {
-   name: `${user.name.first} ${user.name.last}`,
-   money: Math.floor(Math.random() * 1000000)
-   };
-   
-   addData(newUser);
- }
+  const user = data.results[0];
 
- //Double everyones money
- function doubleMoney() {
-   data = data.map((user) => {
-   return { ...user, money: user.money * 2};
-   });
-   updateDOM();
- }
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
 
- // Add new obj to data arr
- function addData(obj) {
-   data.push(obj);
+  addData(newUser);
+}
 
-    updateDOM();
- }
+// Double eveyones money
+function doubleMoney() {
+  data = data.map(user => {
+    return { ...user, money: user.money * 2 };
+  });
 
- // Sort users by richest
- function sortByRichest() {
-   data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
 
-   updateDOM();
- }
+// Sort users by richest
+function sortByRichest() {
+  console.log(123);
+  data.sort((a, b) => b.money - a.money);
 
- // Update Dom
- function updateDOM(provideData = data) {
-   // Clea main div
-   main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+  updateDOM();
+}
 
-   provideData.forEach(item => {
-     const element = document.createElement('div');
-     element.classList.add('person');
-     element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
-       item.money
-       )}`;
-     main.appendChild(element);
-   });
- } 
+// Filter only millionaires
+function showMillionaires() {
+  data = data.filter(user => user.money > 1000000);
 
- // Format number as money
- function formatMoney(number) {
+  updateDOM();
+}
+
+// Calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
+// Add new obj to data arr
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
   return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
- }
+}
 
- // Event Listeners
+// Event listeners
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
